be8ca640239ff402540fcac4f1e29330bf9a5ff4
Nick <barnard.nicholas@googlemail.com>
2019-12-09 11:32:08 +0000

    created migration for comments table

---
+++ db/migrations/20191209112409_create_table_comments.js
@@ -0,0 +1,22 @@
+exports.up = function(knex) {
+  console.log("create_table_comments - UP");
+  return knex.schema.createTable("comments", function(table) {
+    table.increments("comment_id").primary();
+    table
+      .string("author")
+      .references("users.username")
+      .notNullable();
+    table
+      .integer("article_id")
+      .references("articles.article_id")
+      .notNullable();
+    table.integer("votes").defaultsTo("0");
+    table.timestamp("created_at");
+    table.text("body").notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  console.log("create_table_comments - DOWN");
+  return knex.schema.dropTable("comments");
+};
