4d2aaae2d41971af977a23da6742b3e33d8eddc6
Nick <barnard.nicholas@googlemail.com>
2019-12-11 16:42:00 +0000

    experimental landing message

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const server = express();
 const apiRouter = require("./router/r-api");
-const { handlingErrors, sqlErrors, errorCatcher } = require("./error-handlers/error-handlers");
+const {
+  handlingErrors,
+  sqlErrors,
+  errorCatcher
+} = require("./error-handlers/error-handlers");
 
 server.use(express.json());
+
+server.get(
+  "/"((req, res, next) => {
+    res
+      .status(200)
+      .send({ msg: "Welcome to NB-News. GET /api for more information" });
+  })
+);
 
 server.use("/api", apiRouter);
 
